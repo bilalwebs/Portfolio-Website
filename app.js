e97503015c33a1e3cf451c02fc4b1a2726d521da
bilalwebs <bil2023aljutt@gmail.com>
2026-04-22 15:37:03 +0500

    improvment work

--- app.js
+++ app.js
@@ -1,22 +1,57 @@
-var tablinks = document.getElementsByClassName("tab-links");
-var tabcontents = document.getElementsByClassName("tab-contents");
-var sideMenu = document.getElementById("sidemenu");
+const tablinks = document.getElementsByClassName("tab-links");
+const tabcontents = document.getElementsByClassName("tab-contents");
+const sideMenu = document.getElementById("sidemenu");
+const openBtn = document.getElementById("openMenu");
+const closeBtn = document.getElementById("closeMenu");
 
+// Tabs
 function opentab(tabname) {
-  for (tablink of tablinks) {
+  for (let tablink of tablinks) {
     tablink.classList.remove("active-link");
   }
-  for (tabcontent of tabcontents) {
+
+  for (let tabcontent of tabcontents) {
     tabcontent.classList.remove("active-tab");
   }
+
   event.currentTarget.classList.add("active-link");
   document.getElementById(tabname).classList.add("active-tab");
 }
 
-
-function openmenu() {
+// OPEN MENU
+openBtn.addEventListener("click", () => {
   sideMenu.style.right = "0";
-}
-function closemenu() {
+});
+
+// CLOSE MENU
+closeBtn.addEventListener("click", () => {
   sideMenu.style.right = "-200px";
-}
+});
+// Google form submit
+const scriptURL =
+  "https://script.google.com/macros/s/AKfycbxVCkijDfS_Xd7k8wP8zh9Xx3JVXcoaxbZsvUUz0iRzWWHKEiWzaQY9HhxlLYJRYJBB/exec";
+
+const form = document.forms["submit-to-google-sheet"];
+const msg = document.getElementById("msg");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  fetch(scriptURL, {
+    method: "POST",
+    body: new FormData(form),
+  })
+    .then(() => {
+      msg.innerHTML = "Message sent successfully ✅";
+
+      setTimeout(() => {
+        msg.innerHTML = "";
+      }, 3000);
+
+      form.reset();
+    })
+    .catch((error) => {
+      console.log("Error!", error.message);
+      msg.innerHTML = "Something went wrong ❌";
+    });
+});
